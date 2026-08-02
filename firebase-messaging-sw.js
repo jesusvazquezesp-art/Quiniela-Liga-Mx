837e98c5bb43421faf52834c1eb239c4333495d7
jesusvazquezesp-art <jesusvazquezesp@hotmail.com>
2026-08-02 13:13:14 -0600

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,6 +1,8 @@
 importScripts("https://www.gstatic.com/firebasejs/12.17.0/firebase-app-compat.js");
 importScripts("https://www.gstatic.com/firebasejs/12.17.0/firebase-messaging-compat.js");
 
+console.log("🔥 firebase-messaging-sw cargado");
+
 firebase.initializeApp({
   apiKey: "AIzaSyCtEzANH1GWNESY_uNPpHD6MJpwuZkcvl8",
   authDomain: "quiniela-liga-mx-43fe2.firebaseapp.com",
@@ -15,6 +17,7 @@ const messaging = firebase.messaging();
 messaging.onBackgroundMessage((payload)=>{
 
     console.log("Notificación en segundo plano:", payload);
+console.log("📩 Payload recibido:", payload);
 
     self.registration.showNotification(
 
